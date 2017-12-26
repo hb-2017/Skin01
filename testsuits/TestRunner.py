51eed28c1a6b4af73c70110b5e6da4181b518fb3
hb-2017 <819920696@qq.com>
2017-12-26 15:11:46 +0800

    111

--- testsuits/TestRunner.py
+++ testsuits/TestRunner.py
@@ -21,7 +21,7 @@ fp = open(HtmlFile, "wb")
 suite = unittest.TestLoader().discover("testsuits")
 
 if __name__ == '__main__':
-    print('自动化测试开始')
+    # print('自动化测试开始')
     # 初始化一个HTMLTestRunner实例对象，用来生成报告
     runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"skin01项目测试报告", description=u"用例测试情况")
     # 开始执行测试套件
